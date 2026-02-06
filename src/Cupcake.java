1258fd9d5f65879aa4a0e3db09182f44dff14e8c
Devika Sajeesh <73882104+Devika-Sajeesh@users.noreply.github.com>
2026-02-06 10:44:30 +0530

    Refactor Cupcake class to Sunrise with main method

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,9 +1,23 @@
-public class Cupcake {
-    
-        // TODO: Read n
-        // TODO: Read calorie counts into an array
-        // TODO: Sort the array (think about ascending vs descending)
-        // TODO: Calculate the minimum miles using the formula (c * 2^j)
-        // TODO: Print the result (Use 'long' for the sum to avoid overflow)
-    
+import java.util.Scanner;
+
+public class Sunrise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                count++;
+                max = arr[i];
+            }
+        }
+        System.out.println(count);
+    }
 }
