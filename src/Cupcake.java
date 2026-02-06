33c86fcc70adea5c18590a37c810db04119036c9
Devika Sajeesh <73882104+Devika-Sajeesh@users.noreply.github.com>
2026-02-06 10:48:07 +0530

    Refactor Sunrise class to Cupcake with calorie calculation

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
-public class Sunrise {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        sc.close();
-        
-        int count = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] > max) {
-                count++;
-                max = arr[i];
+public class Cupcake {
+    
+        public static void main(String[] args) {        
+            Scanner scanner = new Scanner(System.in);
+    
+            int n = scanner.nextInt();
+            int[] calories = new int[n];
+            for (int i = 0; i < n; i++) {
+                calories[i] = scanner.nextInt();
             }
+    
+            // Sort calories in descending order
+            java.util.Arrays.sort(calories);
+            long minMiles = 0;
+            for (int j = 0; j < n; j++) {
+                minMiles += (long) calories[n - 1 - j] * (1L << j); // c * 2^j
+            }
+    
+            System.out.println(minMiles);
         }
-        System.out.println(count);
-    }
+        
+    
 }
